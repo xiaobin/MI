ccbef9745e4f5f177e2e10a052b226fe2da1cb7c
yesh <454514080@qq.com>
2017-05-10 15:13:55 +0800

    注册中心与监控中心整合

--- mi-api/mi-eureka-server/src/main/java/com/mi/MiEurekaServerApplication.java
+++ mi-api/mi-eureka-server/src/main/java/com/mi/MiEurekaServerApplication.java
@@ -1,14 +1,14 @@
 package com.mi;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
-@EnableDiscoveryClient //发现
+
 @SpringBootApplication
+@EnableEurekaServer
+@EnableDiscoveryClient
 public class MiEurekaServerApplication {
 
 	public static void main(String[] args) {
