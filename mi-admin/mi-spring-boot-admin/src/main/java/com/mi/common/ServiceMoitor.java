f8824d821afc802274e5f0335afa0c7aa70b9919
yesh <454514080@qq.com>
2017-05-11 14:02:04 +0800

    工程子包调整

---
+++ mi-admin/mi-spring-boot-admin/src/main/java/com/mi/common/ServiceMoitor.java
@@ -0,0 +1,46 @@
+package com.mi.common;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.stereotype.Component;
+
+/**
+ * gague和counter的定制 监控
+ * @author yesh
+ *         (M.M)!
+ *         Created by 2017/5/10.
+ */
+@Aspect
+@Component
+public class ServiceMoitor {
+
+    @Autowired
+    private GaugeService gaugeService;
+
+    @Autowired
+    private CounterService counterService;
+
+    @Before(value = "execution(* com.mi.controller.*.*(..))")
+    public void countServiceInvoke(JoinPoint joinPoint) {
+        counterService.increment(joinPoint.getSignature().toString());
+        System.err.println("监控咯"+joinPoint.getSignature());
+    }
+
+    @Around(value = "execution(* com.mi.controller.*.*(..))")
+    public void latencyService(ProceedingJoinPoint pjp) throws Throwable {
+        long start = System.currentTimeMillis();
+//        pjp.proceed();
+//        Thread.sleep(2000);
+        long end = System.currentTimeMillis();
+        System.err.println("延迟多久："+(end-start));
+        gaugeService.submit(pjp.getSignature().toString(), end - start);
+    }
+
+}
--- mi-admin/mi-spring-boot-admin/src/main/java/com/mi/controller/ClientController.java
+++ mi-admin/mi-spring-boot-admin/src/main/java/com/mi/controller/ClientController.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,23 +19,10 @@ public class ClientController {
 
     private final Logger log = Logger.getLogger(ClientController.class);
 
-    private final DiscoveryClient discoveryClient;
 
-    @Autowired
-    public ClientController(DiscoveryClient discoveryClient) {
-        this.discoveryClient = discoveryClient;
-    }
-
-    @RequestMapping("/hi")
+    @RequestMapping("/hii")
     public String hello1(@RequestParam String name){
         System.err.println("-----------------------------");
-        ServiceInstance instance = discoveryClient.getInstances("mi-eureka-client").get(0);
-
-        log.info("Method:---->"+this.getClass().getSimpleName()+"---->Hello");
-        log.info("Url:---->"+instance.getUri());
-        log.info("Host:---->"+instance.getHost());
-        log.info("Port:---->"+instance.getPort());
-
-        return "Hello !!   "+name + " ,I here in port  "+instance.getPort();
+        return "Hello !!   "+name + " ,I here in port  ";
     }
 }
