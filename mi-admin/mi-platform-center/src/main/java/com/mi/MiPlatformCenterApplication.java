557b89971e6a2712c636f362b515c42b0a450597
yesh <454514080@qq.com>
2017-06-13 17:59:34 +0800

    add mi-platform-center

---
+++ mi-admin/mi-platform-center/src/main/java/com/mi/MiPlatformCenterApplication.java
@@ -0,0 +1,12 @@
+package com.mi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiPlatformCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiPlatformCenterApplication.class, args);
+	}
+}
