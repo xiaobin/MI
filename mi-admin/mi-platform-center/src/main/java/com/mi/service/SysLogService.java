88278bd4ef71e1096d3ef3bfbc4712875a40fa38
yesh <454514080@qq.com>
2017-06-21 09:38:53 +0800

    mybatis update to mybatis-plus

---
+++ mi-admin/mi-platform-center/src/main/java/com/mi/service/SysLogService.java
@@ -0,0 +1,14 @@
+package com.mi.service;
+
+import com.baomidou.mybatisplus.service.IService;
+import com.mi.model.SysLog;
+
+/**
+ * 系统日志 服务
+ * @author yesh
+ *         (M.M)!
+ *         Created by 2017/6/21.
+ */
+public interface SysLogService extends IService<SysLog> {
+
+}
