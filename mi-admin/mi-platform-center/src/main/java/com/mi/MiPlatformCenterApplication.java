f6e5e3a29b178f8f2f6534bd0b239e267a668728
yesh <454514080@qq.com>
2017-06-21 14:05:15 +0800

    fix up start class

--- mi-admin/mi-platform-center/src/main/java/com/mi/MiPlatformCenterApplication.java
+++ mi-admin/mi-platform-center/src/main/java/com/mi/MiPlatformCenterApplication.java
@@ -1,12 +1,16 @@
 package com.mi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
-
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
-@ComponentScan
+@ServletComponentScan
+@Configuration
+@EnableAutoConfiguration
 public class MiPlatformCenterApplication {
 
 	public static void main(String[] args) {
