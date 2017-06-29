b95a36690f50137dac494d25012993ed0e2e75e4
yesh <454514080@qq.com>
2017-06-29 21:58:52 +0800

    All Module Update For Swagger2 Use The Common-Module

--- mi-api/mi-eureka-client/src/main/java/com/mi/SwaggerConfig.java
+++ mi-api/mi-eureka-client/src/main/java/com/mi/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.mi;
 
 import com.mi.common.annotation.Dev;
 import com.mi.common.annotation.Pro;
+import com.mi.common.swagger.BaseSwaggerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,54 +27,54 @@ import java.util.Collections;
  */
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig {
+public class SwaggerConfig extends BaseSwaggerConfig {
 
-    @Value("${server.port}")
-    private String serverPort;
-
-    private String contentPath;
-
-    private Docket createApiDoc(String groupName, Class<? extends Annotation> annotation){
-        return  new Docket(DocumentationType.SWAGGER_2)
-                .apiInfo(initApiInfo())
-                .groupName(groupName)
-                .consumes(Collections.singleton(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
-                .produces(Collections.singleton(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .select()
-                .apis(RequestHandlerSelectors.withClassAnnotation(annotation)) //多种方式选择扫描区别分组
-                .paths(PathSelectors.any())
-                .build();
-    }
-
-    @Bean
-    public Docket createRestApi() {
-        return this.createApiDoc("dev", Dev.class);
-    }
-
-    @Bean
-    public Docket createRestAp2i() {
-        return this.createApiDoc("pro",Pro.class);
-    }
-
-    private ApiInfo initApiInfo() {
-        return new ApiInfoBuilder()
-                .title("MI-XX 项目 Platform API")
-                .description(initContextInfo())
-                .version("1.0.0")
-                .termsOfServiceUrl("http://blog.csdn.net/fjnpysh")
-                .contact("MIYAOW")
-                .license("请联系我的博客")
-                .licenseUrl("http://blog.csdn.net/fjnpysh")
-                .build();
-    }
-
-    private String initContextInfo() {
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("REST API 设计在细节上有很多自己独特的需要注意的技巧，并且对开发人员在构架设计能力上比传统 API 有着更高的要求。")
-                .append("<br/>")
-                .append("本文通过翔实的叙述和一系列的范例，从整体结构，到局部细节，分析和解读了为了提高易用性和高效性，REST API 设计应该注意哪些问题以及如何解决这些问题。");
-        return stringBuilder.toString();
-    }
+//    @Value("${server.port}")
+//    private String serverPort;
+//
+//    private String contentPath;
+//
+//    private Docket createApiDoc(String groupName, Class<? extends Annotation> annotation){
+//        return  new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(initApiInfo())
+//                .groupName(groupName)
+//                .consumes(Collections.singleton(MediaType.APPLICATION_FORM_URLENCODED_VALUE))
+//                .produces(Collections.singleton(MediaType.APPLICATION_JSON_UTF8_VALUE))
+//                .select()
+//                .apis(RequestHandlerSelectors.withClassAnnotation(annotation)) //多种方式选择扫描区别分组
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//    @Bean
+//    public Docket createRestApi() {
+//        return this.createApiDoc("dev", Dev.class);
+//    }
+//
+//    @Bean
+//    public Docket createRestAp2i() {
+//        return this.createApiDoc("pro",Pro.class);
+//    }
+//
+//    private ApiInfo initApiInfo() {
+//        return new ApiInfoBuilder()
+//                .title("MI-XX 项目 Platform API")
+//                .description(initContextInfo())
+//                .version("1.0.0")
+//                .termsOfServiceUrl("http://blog.csdn.net/fjnpysh")
+//                .contact("MIYAOW")
+//                .license("请联系我的博客")
+//                .licenseUrl("http://blog.csdn.net/fjnpysh")
+//                .build();
+//    }
+//
+//    private String initContextInfo() {
+//        StringBuilder stringBuilder = new StringBuilder();
+//        stringBuilder.append("REST API 设计在细节上有很多自己独特的需要注意的技巧，并且对开发人员在构架设计能力上比传统 API 有着更高的要求。")
+//                .append("<br/>")
+//                .append("本文通过翔实的叙述和一系列的范例，从整体结构，到局部细节，分析和解读了为了提高易用性和高效性，REST API 设计应该注意哪些问题以及如何解决这些问题。");
+//        return stringBuilder.toString();
+//    }
 
 
 }
--- mi-common/src/main/java/com/mi/MiCommonApplication.java
+++ mi-common/src/main/java/com/mi/MiCommonApplication.java
@@ -1,22 +1,24 @@
 package com.mi;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
+
 @SpringBootApplication
+@Slf4j
 public class MiCommonApplication extends SpringBootServletInitializer {
 
-//	private static Logger log = LoggerFactory.getLogger(MiCommonApplication.class);
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context =  SpringApplication.run(MiCommonApplication.class, args);
-//		String[] activeProfiles = context.getEnvironment().getActiveProfiles();
-//		for (String profile : activeProfiles){
-//			log.info("String active profile:{}" ,profile);
-//		}
-//		log.info("应用程序启动完毕");
+		String[] activeProfiles = context.getEnvironment().getActiveProfiles();
+		for (String profile : activeProfiles){
+			log.info("String active profile:{}" ,profile);
+		}
+		log.info("应用程序启动完毕");
 	}
 }
