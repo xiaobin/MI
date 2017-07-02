1214ae0ca89c33f9c3869783d079be5adf0bb9bc
yesh <454514080@qq.com>
2017-07-02 21:38:12 +0800

    Fix to Maven to Package

--- mi-api/mi-eureka-server/src/main/java/com/mi/MiEurekaServerApplication.java
+++ mi-api/mi-eureka-server/src/main/java/com/mi/MiEurekaServerApplication.java
@@ -13,7 +13,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @EnableEurekaServer
 @SpringBootApplication
 public class MiEurekaServerApplication {
