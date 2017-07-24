bb90eb57c236cf759c262048ad82a96cb413ab58
yesh <454514080@qq.com>
2017-07-24 17:45:08 +0800

    MI

---
+++ mi-api/mi-eureka-client/src/main/java/com/mi/controller/ClientController.java
@@ -0,0 +1,42 @@
+package com.mi.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 服务提供控制器
+ * @author yesh
+ *         (M.M)!
+ *         Created by 2017/5/2.
+ */
+@RestController
+public class ClientController {
+
+    private final Logger log = Logger.getLogger(ClientController.class);
+
+    private final DiscoveryClient discoveryClient;
+
+    @Autowired
+    public ClientController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @RequestMapping("/hi")
+    public String hello1(@RequestParam String name){
+        System.err.println("-----------------------------");
+        ServiceInstance instance = discoveryClient.getInstances("mi-eureka-client").get(0);
+
+        log.info("Method:---->"+this.getClass().getSimpleName()+"---->Hello");
+        log.info("Url:---->"+instance.getUri());
+        log.info("Host:---->"+instance.getHost());
+        log.info("Port:---->"+instance.getPort());
+
+        return "Hello !!   "+name + " ,I here in port  "+instance.getPort();
+    }
+}
